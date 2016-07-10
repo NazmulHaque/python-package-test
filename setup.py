b6df9f93dae9606ac3c87538a2ecf14c7ee11c6b
NewsCred-Nazmul <nazmul@newscred.com>
2016-07-10 16:30:07 +0600

    Test python packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='yourname',
+      version='0.1',
+      description='Type your name',
+      url='http://github.com/nazmulhaque/python-package-test',
+      author='Nazmul Haque',
+      author_email='nazmulhaquedu03@gmail.com',
+      license='Nazmul',
+      packages=['yourname'],
+      zip_safe=False)
